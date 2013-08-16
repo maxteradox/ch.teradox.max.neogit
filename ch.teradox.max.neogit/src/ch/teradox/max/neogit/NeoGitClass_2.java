c683b9a9080fc6c4f4bd6752adcc7af1d083bf1b
Max Teradox <max.teradox@hotmail.com>
2013-08-17 01:53:02 +0200

    Master commit.

--- ch.teradox.max.neogit/src/ch/teradox/max/neogit/NeoGitClass_2.java
+++ ch.teradox.max.neogit/src/ch/teradox/max/neogit/NeoGitClass_2.java
@@ -2,6 +2,7 @@ package ch.teradox.max.neogit;
 
 public class NeoGitClass_2 {
   public static void main(String[] args) {
-    System.out.println("NeoGitClass_1. OK! OK! KO!");
+    System.out.println("NeoGitClass_2. OK! OK! KO!");
+    System.out.println("Branch master");
   }
 }
